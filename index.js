aefb9740bbcddfeecd2b0edb05e1db4586ac4c6b
Hema L M <hemalatha.psna@gmail.com>
2018-12-26 17:26:19 -0600

    Update index.js

--- index.js
+++ index.js
@@ -6,13 +6,13 @@
    function nestedTarget() {
         return document.querySelector('#nested .target');
     }
-    function  deepestChild(){
-      //var nodes = document.querySelectorAll('#grand-node div');
-     
+
+    function  deepestChild(){       
       var child =document.querySelectorAll('#grand-node div div div div');
       var mostDeep = child[child.length- 1];
       return mostDeep;
     }
+
    function increaseRankBy(n) {
         var ranks = document.querySelectorAll('ul.ranked-list li');
         for (var i = 0; i < ranks.length; i++) {
