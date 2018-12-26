d174e422c5b278130f4b8af1c430db1ba4806426
Hemalatha M <hemalatha.psna@gmail.com>
2018-12-26 23:24:08 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 
+   function getFirstSelector(sel) {
+     const first = document.querySelector(`${sel}`);
+     return first;
+   }
+   function nestedTarget() {
+        return document.querySelector('#nested .target');
+    }
+    function  deepestChild(){
+      //var nodes = document.querySelectorAll('#grand-node div');
+     
+      var child =document.querySelectorAll('#grand-node div div div div');
+      var mostDeep = child[child.length- 1];
+      return mostDeep;
+    }
+   function increaseRankBy(n) {
+        var ranks = document.querySelectorAll('ul.ranked-list li');
+        for (var i = 0; i < ranks.length; i++) {
+          ranks[i].innerHTML = parseInt(ranks[i].innerHTML) + n
+          }
+    }
+         
+    
