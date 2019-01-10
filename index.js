4a6042ee6237663c8f09dc27bff0711d96ece74e
Hemalatha M <hemalatha.psna@gmail.com>
2019-01-10 22:40:00 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 
    function getFirstSelector(sel) {
      const first = document.querySelector(`${sel}`);
@@ -21,3 +22,25 @@
     }
          
     
+=======
+ function getFirstSelector(sel) {
+     const first = document.querySelector(`${sel}`);
+     return first;
+   }
+   function nestedTarget() {
+        return document.querySelector('#nested .target');
+    }
+
+    function  deepestChild(){       
+      var child =document.querySelectorAll('#grand-node div div div div');
+      var mostDeep = child[child.length- 1];
+      return mostDeep;
+    }
+
+   function increaseRankBy(n) {
+        var ranks = document.querySelectorAll('ul.ranked-list li');
+        for (var i = 0; i < ranks.length; i++) {
+          ranks[i].innerHTML = parseInt(ranks[i].innerHTML) + n
+          }
+    }
+>>>>>>> 0a8ce34f7e76506d76a012af213463023f181dfa
